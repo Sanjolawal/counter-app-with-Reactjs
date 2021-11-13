6f1d5d039169a7d6998aa04ee460976e54386944
sanjo lawal <sanjolawal600@gmail.com>
2021-11-13 08:47:54 -0800

    yru

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Pratice() {
+  const [text, settext] = useState(78)
+  console.log( text)
+   
+ 
   return(
     <div className="main-height">
-
+    
     </div>
   )
 }
